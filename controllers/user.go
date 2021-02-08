3bd77a286fd8b5214d4083d17b425ca43775fc8e
rkaushik29 <rohit.rk.rk1@gmail.com>
2021-02-08 18:48:00 -0500

    controller funcs

--- controllers/user.go
+++ controllers/user.go
@@ -17,6 +17,67 @@ func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("User Controller connected..."))
 }
 
+func (uc *userController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSON(models.GetUsers(), w)
+}
+
+func (uc *userController) get(id int, w http.ResponseWriter) {
+	u, err := models.getUserByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *userController) post(w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse user object"))
+		return
+	}
+	u, err = models.addUser(u)
+	if err != nil {
+
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *userController) put(id int, w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse user object"))
+		return
+	}
+	if id != u.ID {
+		w.WriteHeader((http.StatusBadRequest))
+		w.Write([]byte("ID's of users do not match"))
+		return
+	}
+	u, err = models.updateUser(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *userController) delete(id int, w http.ResponseWriter) {
+	err := models.removeUserByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
 // Constructor function
 func newUserController() *userController {
 	return &userController{
