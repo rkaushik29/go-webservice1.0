4622fe66c23a08c5e2e31afbb9f829c2c246e520
rkaushik29 <rohit.rk.rk1@gmail.com>
2021-02-07 23:24:58 -0500

    user functionality

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"	"fmt"
+)
+
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,9 +21,41 @@ func getUsers() []*User {
 }
 
 func addUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New User must be included")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 
 	return u, nil
+}
+
+func getUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func updateUser (u User) (User, error) {
+	for i, candidiate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+func removeUserByID (id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:])
+			return nil
+		}
+	}
+	return fmt.Errorf("User with id '%v' not found", id)
 }
